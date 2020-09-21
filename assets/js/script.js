0a824e982dd9e82879076b48069ee7af73387a98
Chelsea Thurman <babefan200@gmail.com>
2020-09-20 22:29:49 -0600

    Search bar added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
+//function for search
+    //store locally
+
+//function for current day display
+
 var weatherDashboard = function() {
     console.log("function");
 };
 
 weatherDashboard();
+
+//function for 5-day forecast
+
+
+//function for search history
+    //display from local storage
